03e76c64b9c07b98f5a9b0beeb3f1cce4aaf0947
blackmage37 <ben.watsaEapW6roXmllon@nfu.org.uk>
2019-01-31 10:28:41 +0000

    attempted fix to handle names with hyphens

--- NSFL/NSFL/Helpers/PlayerHelper.cs
+++ NSFL/NSFL/Helpers/PlayerHelper.cs
@@ -10,37 +10,58 @@ namespace NSFL.Helpers
     {
         public static Player GetPlayerFromPlayerLine(PlayerFileLine playerFileLine)
         {
+            string[] pLine;
+
             try
             {
                 if (playerFileLine.PlayerLine.Split('-').Length < 2 || playerFileLine.PlayerLine.Split(',').Length < 2 || !playerFileLine.PlayerLine.Trim().StartsWith("("))
                 {
                     return new Player(); // Probably an inconsistenly formatted roster title or announcement
+                } else if (playerFileLine.PlayerLine.Split('-').Length > 2)
+                {
+                    // this would suggest there is probably a hyphen in the player name
+                    
+                    // so combine the middle two bits of the split
+                    pLine = playerFileLine.PlayerLine.Split('-');
+                    pLine[1] = pLine[1] + '-' + pLine[2];
+
+                    // shift the last element up one place
+                    pLine[2] = pLine[3];
+
+                    // remove the last element of pLine
+                    pLine = pLine.Take(pLine.Count() - 1).ToArray();
+                        
+                } else
+                {
+                    // this is the expected case, put the parts in an array
+                    pLine = playerFileLine.PlayerLine.Split('-');
                 }
 
-                var player = new Player();
-
-                player.PlayerSeasonDrafted = playerFileLine.PlayerLine.Split(')')[0].Replace("(", "").Trim();
+                var player = new Player
+                {
+                    PlayerSeasonDrafted = playerFileLine.PlayerLine.Split(')')[0].Replace("(", "").Trim()
+                };
 
                 // if the player has a three word name
-                if (playerFileLine.PlayerLine.Split('-')[1].Trim().Split(' ').Length > 2)
+                if (pLine[1].Trim().Split(' ').Length > 2)
                 {
-                    player.PlayerFirstName = playerFileLine.PlayerLine.Split('-')[1].Trim().Split(' ')[0].Trim();
-                    player.PlayerFirstName = player.PlayerFirstName + " " + playerFileLine.PlayerLine.Split('-')[1].Trim().Split(' ')[1].Trim();
-                    player.PlayerLastName = playerFileLine.PlayerLine.Split('-')[1].Trim().Split(' ')[2].Trim();
+                    player.PlayerFirstName = pLine[1].Trim().Split(' ')[0].Trim();
+                    player.PlayerFirstName = player.PlayerFirstName + " " + pLine[1].Trim().Split(' ')[1].Trim();
+                    player.PlayerLastName = pLine[1].Trim().Split(' ')[2].Trim();
                 }
                 // standard firstname lastname
-                else if (playerFileLine.PlayerLine.Split('-')[1].Trim().Split(' ').Length > 1)
+                else if (pLine[1].Trim().Split(' ').Length > 1)
                 {
-                    player.PlayerFirstName = playerFileLine.PlayerLine.Split('-')[1].Trim().Split(' ')[0].Trim();
-                    player.PlayerLastName = playerFileLine.PlayerLine.Split('-')[1].Trim().Split(' ')[1].Trim();
+                    player.PlayerFirstName = pLine[1].Trim().Split(' ')[0].Trim();
+                    player.PlayerLastName = pLine[1].Trim().Split(' ')[1].Trim();
                 }
                 else // A last name is not required, prevent this player from being ommitted
                 {
-                    player.PlayerFirstName = playerFileLine.PlayerLine.Split('-')[1].Trim().Split(' ')[0].Trim();
+                    player.PlayerFirstName = pLine[1].Trim().Split(' ')[0].Trim();
                     player.PlayerLastName = "";
                 }
                 player.PlayerTPE = int.Parse(playerFileLine.PlayerLine.Split(',')[1].Split(':').Last().Trim());
-                player.PlayerPosition = playerFileLine.PlayerLine.Split('-').Last().Split(',')[0].Trim();
+                player.PlayerPosition = pLine.Last().Split(',')[0].Trim();
                 player.PlayerProfileURL = playerFileLine.PlayerLine.Split(',')[2].Trim();
 
                 return player;
