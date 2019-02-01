0a310d762f94a80d46b46923e06ece01acb248ec
blackmage37 <ben.watsaEapW6roXmllon@nfu.org.uk>
2019-02-01 12:06:50 +0000

    minor cosmetic fixes

--- NSFL/NSFL/Helpers/DateTimeHelper.cs
+++ NSFL/NSFL/Helpers/DateTimeHelper.cs
@@ -13,7 +13,7 @@ namespace NSFL.Helpers
         public static string GetDateTimeFromFile(string file)
         {
             string[] sDateTime = System.IO.File.ReadAllLines(System.Web.HttpContext.Current.Server.MapPath(file));
-
+            sDateTime[0] = sDateTime[0].Replace(";", "");
             return sDateTime[0];
         }
 
